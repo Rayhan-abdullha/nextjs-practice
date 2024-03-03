7e1dfd5bc4751dea3ee1c6a3f9fee7021cd112d8
Rayhan-abdullha <captainrayhan215@gmail.com>
2024-03-03 20:00:05 +0600

    error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello next js</h1>
-      <Image src='https://q-xx.bstatic.com/xdata/images/hotel/max750/80400793.jpg?k=a25286921f81153b87996f52e8f3fa85fe59c3bae51424d3c1623f7805f5c380&o=' alt='not found' width={500} height={500} placeholder="blur" />
+    <main className="w-[1200px] m-auto">
+      <h1 className="mb-5">Hello next js</h1>
+      <Image src='https://q-xx.bstatic.com/xdata/images/hotel/max750/80400793.jpg?k=a25286921f81153b87996f52e8f3fa85fe59c3bae51424d3c1623f7805f5c380&o=' alt='not found' width={500} height={500} />
     </main>
   );
 }
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -29,24 +29,24 @@ interface User {
 function getUser() {
     return fetch("https://jsonplaceholder.typicode.com/users", { cache: 'no-cache' }).then(res => res.json())
 }
-
-
 const userPage = async () => {
     const users: User[] = await getUser()
     return (
         <div>
-            <h1 className='text-[30px] font-semibold mb-5'>TODO:</h1>
-            {
-                users.map((user: User) => (
-                    <div key={user.id}>
-                        <Link href={`/user/${user.id}`} >
-                            <h3 className='text-[25px] font-medium'>{user.name}</h3>
-                        </Link>
+            <h1 className='text-[30px] font-semibold mb-5'>Users:</h1>
+            <section className='grid grid-cols-3 gap-2'>
+                {
+                    users.map((user: User) => (
+                        <div key={user.id} className='border border-y-red-950 p-2'>
+                            <Link href={`/user/${user.id}`} >
+                                <h3 className='text-[25px] font-medium'>{user.name}</h3>
+                            </Link>
 
-                        <p>{user.email}</p>
-                    </div>
-                ))
-            }
+                            <p>{user.email}</p>
+                        </div>
+                    ))
+                }
+            </section>
         </div>
     )
 }
